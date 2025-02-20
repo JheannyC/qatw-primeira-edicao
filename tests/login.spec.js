7b4d857e893bbd6fa345da7020aeb73432eab700
Jeane <jeanecardoso@MacBook-Pro-de-Jeane.local>
2025-02-20 07:42:29 -0300

    Refatorando testes da página de login e dashboard

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { obterCodigo2FA } from '../support/db';
 import { LoginPage } from '../pages/LoginPage'
+import { DashPage } from '../pages/DashPage';
 
 test('Não deve logar quando o código de autenticação é inválido', async ({ page }) => {
   const loginPage = new LoginPage(page)
@@ -22,6 +23,7 @@ test('Não deve logar quando o código de autenticação é inválido', async ({
 test('Deve acessar a conta do usuário', async ({ page }) => {
 
   const loginPage = new LoginPage(page)
+  const dashPage = new DashPage(page)
 
   const usuario = {
     cpf: '00000014141',
@@ -37,10 +39,9 @@ test('Deve acessar a conta do usuário', async ({ page }) => {
 
   const codigo = await obterCodigo2FA()
   await loginPage.informa2FA(codigo)
-
-  await page.waitForTimeout(2000)
   //temporário
-  expect(await loginPage.obterSaldo()).toHaveText('R$ 5.000,00')
-
+  await page.waitForTimeout(2000)
   
+  expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
+
 });
